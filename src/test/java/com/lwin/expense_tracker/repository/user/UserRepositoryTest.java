333e255fc765a1d7d49a27c95b460b7d0decfe80
Kyaw Thit Lwin <kyaw.thitlwin.me@gmail.com>
2023-07-31 02:45:40 +0800

    Unit tests for UserRepository

--- src/test/java/com/lwin/expense_tracker/repository/user/UserRepositoryTest.java
+++ src/test/java/com/lwin/expense_tracker/repository/user/UserRepositoryTest.java
@@ -10,6 +10,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -19,7 +22,7 @@ public class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    public void UserRepository_AddUser_ReturnString () {
+    public void UserRepository_AddUser_ReturnNewUser () {
         User user = User.builder()
                 .email("test@expensetracker.com")
                 .roles("ROLE_USER")
@@ -28,5 +31,59 @@ public class UserRepositoryTest {
         User newUser = userRepository.save(user);
 
         Assertions.assertThat(newUser).isNotNull();
+        Assertions.assertThat(newUser.getEmail()).isEqualTo("test@expensetracker.com");
+    }
+
+    @Test
+    public void UserRepository_GetAllUsers_ReturnUserList () {
+        User user1 = User.builder()
+                .email("test1@expensetracker.com")
+                .roles("ROLE_USER")
+                .password("Password")
+                .build();
+        User user2 = User.builder()
+                .email("test2@expensetracker.com")
+                .roles("ROLE_USER")
+                .password("Password")
+                .build();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        List<User> users = userRepository.findAll();
+
+        Assertions.assertThat(users).isNotNull();
+        Assertions.assertThat(users.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void UserRepository_FindById_ReturnUser () {
+        User user = User.builder()
+                .email("test@expensetracker.com")
+                .roles("ROLE_USER")
+                .password("Password")
+                .build();
+        userRepository.save(user);
+
+        Optional<User> result = userRepository.findById(user.getId());
+
+        Assertions.assertThat(result.isPresent()).isTrue();
+        Assertions.assertThat(result.get()).isNotNull();
+        Assertions.assertThat(result.get().getId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    public void UserRepository_FindByEmail_ReturnUser () {
+        User user = User.builder()
+                .email("test@expensetracker.com")
+                .roles("ROLE_USER")
+                .password("Password")
+                .build();
+        userRepository.save(user);
+
+        Optional<User> result = userRepository.findByEmail(user.getEmail());
+        Assertions.assertThat(result.isPresent()).isTrue();
+        Assertions.assertThat(result.get()).isNotNull();
+        Assertions.assertThat(result.get().getEmail()).isEqualTo(user.getEmail());
     }
 }
